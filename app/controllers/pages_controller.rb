8ab08c8ab6fe19cf461b21996376569e27cc3aed
Marie Begue <marie.prandstetter@gmail.com>
2019-11-26 14:58:03 +0100

    user authentification conditions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home] # pas sûre de cette commande
 
   def home
+  end
+
+  def search
+    redirect_to my_chat_rooms_path unless curent_user.chat_rooms.empty?
   end
 end

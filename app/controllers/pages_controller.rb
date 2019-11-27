6b3c8f6d8c400795c58f91940c98414e5dfa8e8d
Marie Begue <marie.prandstetter@gmail.com>
2019-11-27 11:32:03 +0100

    je dois merger sinon ça pète

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home] # pas sûre de cette commande
 
   def home
-    @chat_rooms = ChatRoom.first
   end
 
   def search
-    raise
     # redirect_to my_chat_rooms_path unless current_user.chat_rooms.empty?
+    # db tag chercher celui qui correspond chat_rooms_tag.where(tag.name == query)
   end
 end

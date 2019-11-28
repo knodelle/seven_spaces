16da23e98ee54f5ffbc8b91e16baa655146fbf69
Marie Begue <marie.prandstetter@gmail.com>
2019-11-28 19:09:26 +0100

    to be continued

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,6 +1,8 @@
 class ChatRoom < ApplicationRecord
   has_many :messages
-  has_many :chat_room_tags
+  has_many :chat_room_tags, inverse_of: :chat_room
   has_many :tags, through: :chat_room_tags
   has_many :subscriptions, dependent: :destroy
+  accepts_nested_attributes_for :chat_room_tags,
+                                allow_destroy: true
 end
--- app/models/chat_room_tag.rb
+++ app/models/chat_room_tag.rb
@@ -1,4 +1,4 @@
 class ChatRoomTag < ApplicationRecord
-  belongs_to :chat_room
+  belongs_to :chat_room, inverse_of: :chat_room_tags
   belongs_to :tag
 end

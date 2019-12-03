7e1d12be074a7bc01007a0a5bdb042a5501dc890
Thibaud Garnier <thibaud.garnier@yahoo.fr>
2019-12-03 09:16:04 +0100

    home2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home] # pas sure de cette commande
+  layout "home"
 
   def home
     @chat_rooms = ChatRoom.first

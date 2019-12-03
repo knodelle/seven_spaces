6671c72d10fc2b2c3a6c1d4418052759e6034b16
Marie Begue <marie.prandstetter@gmail.com>
2019-12-03 10:24:35 +0100

    work in progress

--- app/controllers/chat_room_tags_controller.rb
+++ app/controllers/chat_room_tags_controller.rb
@@ -17,20 +17,20 @@ class ChatRoomTagsController < ApplicationController
 
   # to do plus tard, rattacher à la view par du JS, hover
 
-  def edit
-    @chat_room_tag = ChatRoomTag.find(params[:tag_id])
-  end
+  # def edit
+  #   @chat_room_tag = ChatRoomTag.find(params[:tag_id])
+  # end
 
-  def update
-    # @chat_room_tag = ChatRoomTag.new(chat_room_tag_params)
-    # @chat_room_tag.chat_room = @chat_room
-    @chat_room_tag = ChatRoomTag.find(params[:tag_id])
-    if @chat_room_tag.update_attributes(chat_room_tag_allowed_params)
-      redirect_to my_chat_rooms_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   # @chat_room_tag = ChatRoomTag.new(chat_room_tag_params)
+  #   # @chat_room_tag.chat_room = @chat_room
+  #   @chat_room_tag = ChatRoomTag.find(params[:tag_id])
+  #   if @chat_room_tag.update_attributes(chat_room_tag_allowed_params)
+  #     redirect_to my_chat_rooms_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     @chat_room_tag.destroy

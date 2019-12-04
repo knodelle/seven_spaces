27d4fb76046ad4542fd295ac3060f6101f2c02dd
Adrien Peres <adrien.peres@outlook.fr>
2019-12-04 15:14:06 +0100

    ça me saoule

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   devise_for :users
   get '/search', to: 'pages#search'
 
